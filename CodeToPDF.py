3222a039e27f0daeb764d3fcdb4ba29c6e570b8c
imomochi <lion.toaster@gmail.com>
2021-07-24 18:09:15 +0900

    差分チェック

--- CodeToPDF.py
+++ CodeToPDF.py
@@ -1,10 +1,23 @@
-import tkinter
+import subprocess        # import文なので次以降の例では省略します
+import csv
 
-# Tkクラス生成
-frm = tkinter.Tk()
-# 画面サイズ
-frm.geometry('600x400')
-# 画面タイトル
-frm.title('サンプル画面')
-# 画面をそのまま表示
-frm.mainloop()
+
+subprocess.run([r'C:/Program Files/WinMerge/WinMergeU.exe', \
+    'target/Before', \
+    'target/After', \
+	'-minimize', \
+	'-noninteractive', \
+	'-noprefs', \
+	'-cfg', 'Settings/DirViewExpandSubdirs=1', \
+	'-cfg', 'ReportFiles/ReportType=0', \
+	'-cfg', 'ReportFiles/IncludeFileCmpReport=1', \
+	'-r', \
+	'-u', \
+	'-or', 'target/out.html'])
+
+with open('target/out.html') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+
+        
