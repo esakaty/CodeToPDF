3104c3a03a4de6b400aabe15d13fedc63d215105
imomochi <lion.toaster@gmail.com>
2021-07-25 17:25:20 +0900

    横幅指定追加

--- CodeToPDF.py
+++ CodeToPDF.py
@@ -1,11 +1,16 @@
+#フォルダ内を比較して不一致ファイルを一旦htmlにしてexcelで開いてPDFでエクスポートする。
+
+
 import subprocess
 import csv
 import os
 import shutil
 from enum import Enum
+
 #コマンドプロント上で以下を実行しインストールする。
 #python -m pip install pywin32
 #バージョンアップも必要かも
+#python -m pip install pywin32 -U
 import win32com.client 
 
 path_Null = 'null.txt'
@@ -70,27 +75,27 @@ def main():
             if (DiffList[i][5] != "") & (DiffList[i][2] != tag_File_Same):
                 #サブフォルダのパスとフォルダ出力用のサブフォルダ名生成
                 if(DiffList[i][1] != ''):
-                    subfolder = '\\'+DiffList[i][1]
+                    subfolder     = '\\'+DiffList[i][1]
                     subfoldername = subfolder+'＞'
                 else:
-                    subfolder = ''
+                    subfolder     = ''
                     subfoldername = ''
 
                 #比較ファイルのパス生成
                 #片方しかない場合はnull.txtと比較する。
                 if(DiffList[i][2].startswith(tag_File_OnlyAfter)):
-                    path_File_After      = abspath_After +subfolder+'\\' +DiffList[i][0]
-                    path_File_Before     = abspath_Null
+                    path_File_After  = abspath_After  + subfolder+'\\' + DiffList[i][0]
+                    path_File_Before = abspath_Null
                 elif(DiffList[i][2].startswith(tag_File_OnlyBefore)):
-                    path_File_After      = abspath_Null
-                    path_File_Before     = abspath_Before+subfolder+'\\' +DiffList[i][0]
+                    path_File_After  = abspath_Null
+                    path_File_Before = abspath_Before + subfolder+'\\' + DiffList[i][0]
                 else:
-                    path_File_After      = abspath_After +subfolder+'\\' +DiffList[i][0]
-                    path_File_Before     = abspath_Before+subfolder+'\\' +DiffList[i][0]
+                    path_File_After  = abspath_After  + subfolder+'\\' + DiffList[i][0]
+                    path_File_Before = abspath_Before + subfolder+'\\' + DiffList[i][0]
 
                 #出力ファイルパス生成
-                path_File_Report     = abspath_OutputTmp+subfoldername+DiffList[i][0]+'.html'
-                path_File_PDF        = abspath_Output+subfoldername+DiffList[i][0]+'.pdf'
+                path_File_Report     = abspath_OutputTmp + subfoldername + DiffList[i][0] + '.html'
+                path_File_PDF        = abspath_Output    + subfoldername + DiffList[i][0] + '.pdf'
 
                 #比較レポート生成(csvファイル生成)
                 MakeDiff_ReportFile( \
@@ -116,10 +121,18 @@ def MakeDiff_ReportFile(Before,After,Output):
 #HTML→PDF
 def HtmlToPDF(HtmlFile,PDFFile):
 
-    #テスト:PDF出力
+    #htmlをエクセルで開く
     excel = win32com.client.Dispatch("Excel.Application")
     file = excel.Workbooks.Open(HtmlFile, UpdateLinks=0, ReadOnly=True)
     file.WorkSheets(1).Select()
+
+    #横幅調整
+    file.WorkSheets(1).Columns(1).ColumnWidth = 5
+    file.WorkSheets(1).Columns(2).ColumnWidth = 100
+    file.WorkSheets(1).Columns(3).ColumnWidth = 5
+    file.WorkSheets(1).Columns(4).ColumnWidth = 100
+
+    #印刷設定
     file.WorkSheets(1).Pagesetup.Zoom  = False
     file.WorkSheets(1).Pagesetup.Orientation = 2
     file.WorkSheets(1).Pagesetup.FitToPagesWide = 1
